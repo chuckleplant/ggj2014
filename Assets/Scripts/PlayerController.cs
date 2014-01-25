fb60bacf8e580d5587541b2df4ed52f8c3f1d8f5
Underskay <julian.ramos.m@gmail.com>
2014-01-25 16:03:50 +0100

    added sprite scene with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,41 +3,61 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float 	maxSpeed = 10f;
-	bool			facingRight = true;
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+	public bool facingUp = false;
+
 	public Camera m_Camera;
 
 	public Animator anim;
 
+	public float horizontalMove;
+	public float verticalMove;
+
 	// Use this for initialization
 	void Start () {
 		//anim = GameObject.Find ("Idle").animation;
-		//anim = GetComponent<Animator>();
+		anim = GetComponent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float horizontalMove 	= Input.GetAxis("Horizontal");
-		float verticalMove 		= Input.GetAxis("Vertical");
+		horizontalMove 	= Input.GetAxis("Horizontal");
+		verticalMove 		= Input.GetAxis("Vertical");
 
 		rigidbody.velocity = new Vector3(horizontalMove, 0f, verticalMove) * maxSpeed;
 
-		//anim.SetFloat("Speed", rigidbody.velocity.magnitude);
 
-		if (verticalMove > 0 && !facingRight)
-			FlipVertical();
-		else if (verticalMove < 0 && facingRight)
-			FlipVertical();
+
+		if (horizontalMove > 0 && !facingRight) {
+			FlipHorizontal();
+		}else if (horizontalMove < 0 && facingRight) {
+			FlipHorizontal ();
+		}
+
+		if (verticalMove > 0 && !facingUp) {
+			facingUp = !facingUp;
+		}else if (verticalMove < 0 && facingUp) {
+			facingUp = !facingUp;
+		}
+
+		//animSets
+		anim.SetFloat ("horizontalSpeed", Mathf.Abs(horizontalMove));
+		anim.SetFloat ("verticalSpeed", Mathf.Abs(verticalMove));
+		anim.SetBool ("facingUp", facingUp);
+
+
 	}
 
 
 
-	void FlipVertical()
+	void FlipHorizontal()
 	{
 		facingRight 			= !facingRight;
 		Vector3 theScale 		= transform.localScale;
-		theScale.z				*= -1;
+		theScale.x				*= -1;
 		transform.localScale 	= theScale;
+
 	}
 }
